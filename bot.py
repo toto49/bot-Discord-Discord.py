cab98904ca14fa27fbfa7c3c9eb0abd79d160900
atom497 <66869653+toto49@users.noreply.github.com>
2021-06-12 22:39:37 +0200

    Event de demarrage

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ from discord.ext import commands, tasks
 #determiner le préfixe du bot
 bot = commands.Bot(command_prefix = "!", description = "Bot de toto49")
 
+@bot.event
+async def on_ready():
+  print("le bot est en ligne")
+
+
 
 
 
