3a938ba9c708eb533f408e05f492f5a6f78ae3e5
atom497 <66869653+toto49@users.noreply.github.com>
2021-06-13 18:22:56 +0200

    Code

--- Commande simple.py
+++ Commande simple.py
@@ -1,1 +1,10 @@
-
+@bot.command()
+#Définir si c'est une commande, un event ou autre chose
+async def github(ctx):
+# définir le nom de la commande eet des requêtes ()
+    await ctx.send("bonjour")
+#envoie bonjour
+    embed = discord.Embed(title = " github ", description = " je m appelle github et je suis un site internet ")
+    embed.add_field(name = "github", value = "au revoir")
+    await ctx.send(embed = embed)
+# envoie l'embed
