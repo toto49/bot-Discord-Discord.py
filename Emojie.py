463ce1276391de61e87ed87b4ac088fb9a4610ad
atom497 <66869653+toto49@users.noreply.github.com>
2021-06-14 18:50:35 +0000

    Update Emojie.py

--- Emojie.py
+++ Emojie.py
@@ -3,5 +3,7 @@
 async def github(ctx):
 # définir le nom de la commande eet des requêtes ()
     message =await ctx.send("bonjour")
-    await ctx message.add_reaction("")
-    #envoie bonjour
+    # definir le bonjour pour ajouter une reaction
+    await message.add_reaction("<a:valid:794520750802731020>")
+    # envoie une reaction en dessous du message
+    
