eca3dd46d619c76c200ba7c59ae0afa3581eb73c
atom497 <66869653+toto49@users.noreply.github.com>
2021-06-14 18:44:24 +0000

    Create Emojie.py

---
+++ Emojie.py
@@ -0,0 +1,7 @@
+@bot.command()
+#Définir si c'est une commande, un event ou autre chose
+async def github(ctx):
+# définir le nom de la commande eet des requêtes ()
+    message =await ctx.send("bonjour")
+    await ctx message.add_reaction("")
+    #envoie bonjour
