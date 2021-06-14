9f12e5e9683aa7e1a94239bda9b3386ede42d559
atom497 <66869653+toto49@users.noreply.github.com>
2021-06-14 18:13:09 +0000

    Update Commande simple.py

--- Commande simple.py
+++ Commande simple.py
@@ -3,9 +3,10 @@
 async def github(ctx):
 # définir le nom de la commande eet des requêtes ()
     await ctx.send("bonjour")
-#envoie bonjour
+    #envoie bonjour
     embed = discord.Embed(title = " github ", description = " je m appelle github et je suis un site internet ")
     # genere un embed avec un titre et une description
     embed.add_field(name = "github", value = "au revoir")
+    # ajoute un sous titre et une description
     await ctx.send(embed = embed)
-# envoie l'embed
+    # envoie l'embed
