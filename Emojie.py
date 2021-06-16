b4add38d9181af9bdc61636207fa63c0b786727c
atom497 <66869653+toto49@users.noreply.github.com>
2021-06-16 20:41:30 +0200

    Delete Emojie.py

--- Emojie.py
+++
@@ -1,9 +0,0 @@
-@bot.command()
-#Définir si c'est une commande, un event ou autre chose
-async def github(ctx):
-# définir le nom de la commande eet des requêtes ()
-    message =await ctx.send("bonjour")
-    # definir le bonjour pour ajouter une reaction
-    await message.add_reaction("<a:valid:794520750802731020>")
-    # envoie une reaction en dessous du message
-    
